b07e0353efb4cf7beb034e41f51f16b5c1e2df4f
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-06-27 12:01:53 -0700

    week10

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-require('dotenv').config()
-const { Pool } = require('pg')
-
+require('dotenv').config();
+const { Pool } = require('pg');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+const bodyParser = require('body-parser');
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
+app.post('/costcalculator', (req, res) => {
+
+    // Set up a JSON object of the values we want to pass along to the EJS result page
+    const params = {cost, weight, mailtype};
+
+    console.log(params)
+    res.render('pages/costcalculator', params)
+})   
+
 var sql = "SELECT * FROM siteuser";
 pool.query(sql, function(err, result) {
     // If an error occurred...
@@ -14,3 +32,6 @@ pool.query(sql, function(err, result) {
     console.log("Back from DB with result:");
     console.log(result.rows);
 });  
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
