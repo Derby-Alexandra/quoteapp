e35b1d733b607163c8f22b6c3b180cb7241ab6f8
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2020-06-27 19:32:43 -0700

    week 10

--- server.js
+++ server.js
@@ -1,37 +1,57 @@
-require('dotenv').config();
-const { Pool } = require('pg');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const app = express();
-const bodyParser = require('body-parser');
-const connectionString = process.env.DATABASE_URL;
+require('dotenv').config()
+const { Pool } = require('pg')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express()
+const bodyParser = require('body-parser')
+const connectionString = process.env.DATABASE_URL
 const pool = new Pool({connectionString: connectionString})
+const https = require('https');
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
-app.post('/costcalculator', (req, res) => {
-
-    // Set up a JSON object of the values we want to pass along to the EJS result page
-    const params = {cost, weight, mailtype};
-
-    console.log(params)
-    res.render('pages/costcalculator', params)
-})   
-
-var sql = "SELECT * FROM siteuser";
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result.rows);
-});  
+app.get('/', (req, res) => {
+    get_quote(function(quotes) {
+        let random_index = Math.floor((Math.random() * quotes.length) + 1)
+        res.render('index', {quote: quotes[random_index].text, author: quotes[random_index].author})
+    })
+})
+app.get('/generate', (req, res) => {
+    get_quote(function(quotes) {
+        let random_index = Math.floor((Math.random() * quotes.length) + 1)
+        res.render('index', {quote: quotes[random_index].text, author: quotes[random_index].author})
+    })
+})
+   
+//
+//var sql = "SELECT * FROM siteuser";
+//pool.query(sql, function(err, result) {
+//    // If an error occurred...
+//    if (err) {
+//        console.log("Error in query: ")
+//        console.log(err)
+//    }
+//    // Log this to the console for debugging purposes.
+//    console.log("Back from DB with result:")
+//    console.log(result.rows)
+//});  
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function get_quote(callback) {
+    https.get("https://type.fit/api/quotes", res => {
+      res.setEncoding("utf8")
+      let body = ""
+      res.on("data", data => {
+        body += data
+      })
+      res.on("end", () => {
+        body = JSON.parse(body)
+        callback(body)
+      })
+    })
+}
